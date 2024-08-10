085df95cdb3115e2824359defc1cd1b416ff14fc
Zuveriya Munshi <zuveriyamunshi924@gmail.com>
2024-08-10 23:02:22 +0530

    Unused Removed

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,10 +1,10 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl,githubUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, githubUrl }) => {
   return (
     <Col size={20} sm={5} md={6}>
       <div className="proj-imgbx">
-        <img src={imgUrl} />
+        <img src={imgUrl} alt={title}/>
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -138,7 +138,7 @@ export const Projects = () => {
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
+      <img className="background-image-right" src={colorSharp2} alt="backgound-img"></img>
     </section>
   )
 }
